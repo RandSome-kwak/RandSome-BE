288cf960ced50266e95ff3372dcd5e5258d3ea83
kwakseobang <iii6602@naver.com>
2025-09-27 01:07:26 +0900

    Feat[#5]: 로그인 아이디 및 닉네임 중복 체크 기능 추가

--- src/test/java/org/kwakmunsu/randsome/domain/member/controller/MemberControllerTest.java
+++ src/test/java/org/kwakmunsu/randsome/domain/member/controller/MemberControllerTest.java
@@ -15,6 +15,7 @@ import org.kwakmunsu.randsome.domain.member.MemberFixture;
 import org.kwakmunsu.randsome.domain.member.controller.dto.MemberRegisterRequest;
 import org.kwakmunsu.randsome.domain.member.enums.Gender;
 import org.kwakmunsu.randsome.domain.member.enums.Mbti;
+import org.kwakmunsu.randsome.domain.member.serivce.dto.CheckResponse;
 import org.kwakmunsu.randsome.domain.member.serivce.dto.MemberRegisterServiceRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -86,6 +87,38 @@ class MemberControllerTest extends ControllerTestSupport {
                         .content(requestJson))
                 .apply(print())
                 .hasStatus(HttpStatus.BAD_REQUEST);
+    }
+
+    @DisplayName("로그인 아이디 중복 체크를 한다")
+    @Test
+    void checkLoginId() {
+        // given
+        var loginId = "testLoginId";
+        given(memberService.isLoginIdAvailable(loginId)).willReturn(new CheckResponse(true));
+
+        // when & then
+        assertThat(mvcTester.get().uri("/api/v1/members/check-login-id")
+                .param("loginId", loginId))
+                .apply(print())
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.available").isEqualTo(true);
+    }
+
+    @DisplayName("닉네임 중복 체크를 한다")
+    @Test
+    void checkNickname() {
+        // given
+        var nickname = "testNickname";
+        given(memberService.isNicknameAvailable(nickname)).willReturn(new CheckResponse(true));
+
+        // when & then
+        assertThat(mvcTester.get().uri("/api/v1/members/check-nickname")
+                .param("nickname", nickname))
+                .apply(print())
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.available").isEqualTo(true);
     }
 
 }
