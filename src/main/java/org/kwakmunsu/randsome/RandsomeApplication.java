01fe9872b5d3bb1f418ecb90dadefa65dda6c33b
kwakseobang <iii6602@naver.com>
2025-09-26 15:39:42 +0900

    git initailze

---
+++ src/main/java/org/kwakmunsu/randsome/RandsomeApplication.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.randsome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandsomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandsomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kwakmunsu/randsome/RandsomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.randsome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandsomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
