5377403f0b59447056f78a0124834f81124b3690
kwakseobang <iii6602@naver.com>
2025-09-29 12:33:08 +0900

    Test[#10]: 테스트 전용 TestMember annotation and security context 생성

---
+++ src/test/java/org/kwakmunsu/randsome/global/TestSecurityConfig.java
@@ -0,0 +1,35 @@
+package org.kwakmunsu.randsome.global;
+
+import lombok.RequiredArgsConstructor;
+import org.kwakmunsu.randsome.domain.member.enums.Role;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@TestConfiguration
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/api/v1/members/sign-up",
+                                "/api/v1/members/check-login-id",
+                                "/api/v1/members/check-nickname"
+                                ).permitAll()   // 로그인, 회원가입, 토큰 재발급 등은 인증 불필요
+                        .anyRequest().hasAnyAuthority(Role.ROLE_MEMBER.name(), Role.ROLE_CANDIDATE.name())
+                );
+
+        return http.build();
+    }
+
+}
