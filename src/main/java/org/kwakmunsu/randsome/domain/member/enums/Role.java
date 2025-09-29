d388e0ecfc38cd5ce08d774c4a4b180f37e294cd
kwakseobang <iii6602@naver.com>
2025-09-29 11:51:19 +0900

    Feat[#10]: Role enum에 한국어 이름 필드 추가

--- src/main/java/org/kwakmunsu/randsome/domain/member/enums/Role.java
+++ src/main/java/org/kwakmunsu/randsome/domain/member/enums/Role.java
@@ -1,9 +1,17 @@
 package org.kwakmunsu.randsome.domain.member.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
-    ROLE_GUEST,
-    ROLE_MEMBER,
-    ROLE_CANDIDATE,
-    ROLE_ADMIN,
+    ROLE_GUEST     ("손님"),
+    ROLE_MEMBER    ("회원"),
+    ROLE_CANDIDATE ("매칭 후보"),
+    ROLE_ADMIN     ("관리자"),
     ;
+
+    private final String value;
+
 }
