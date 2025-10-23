6865d8f79d36d53876323f02d38c28c8df02f631
kwakseobang <iii6602@naver.com>
2025-10-23 09:28:26 +0900

    Feat/[#84]: Disable performance test classes for data initializers

--- src/test/java/org/kwakmunsu/randsome/admin/PaymentDataInitializer.java
+++ src/test/java/org/kwakmunsu/randsome/admin/PaymentDataInitializer.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Random;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.kwakmunsu.randsome.PerformanceTestSupport;
 import org.kwakmunsu.randsome.domain.payment.enums.PaymentStatus;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+@Disabled
 public class PaymentDataInitializer extends PerformanceTestSupport {
 
     @Autowired
