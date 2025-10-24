c451b5b676a83950f8484ad891115005d1178a63
kwakseobang <iii6602@naver.com>
2025-10-24 09:47:48 +0900

    Refactor[#92]: Rename MatchingApplicationAdminRepositoryImpl to MatchingApplicationAdminRepository and update imports for consistency

--- src/main/java/org/kwakmunsu/randsome/admin/matching/repository/matchingapplication/MatchingApplicationAdminRepository.java
+++ src/main/java/org/kwakmunsu/randsome/admin/matching/repository/matchingapplication/MatchingApplicationAdminRepository.java
@@ -3,7 +3,6 @@ package org.kwakmunsu.randsome.admin.matching.repository.matchingapplication;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.kwakmunsu.randsome.admin.matching.repository.dto.MatchingApplicationAdminPreviewResponse;
-import org.kwakmunsu.randsome.admin.matching.service.MatchingApplicationAdminRepository;
 import org.kwakmunsu.randsome.domain.EntityStatus;
 import org.kwakmunsu.randsome.domain.matching.entity.MatchingApplication;
 import org.kwakmunsu.randsome.domain.matching.enums.MatchingStatus;
@@ -13,23 +12,20 @@ import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
-public class MatchingApplicationAdminRepositoryImpl implements MatchingApplicationAdminRepository {
+public class MatchingApplicationAdminRepository {
 
     private final MatchingApplicationAdminJpaRepository matchingApplicationAdminJpaRepository;
     private final MatchingApplicationQueryDslRepository matchingApplicationQueryDslRepository;
 
-    @Override
     public MatchingApplication findById(Long id) {
         return matchingApplicationAdminJpaRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_MATCHING_APPLICATION));
     }
 
-    @Override
     public List<MatchingApplicationAdminPreviewResponse> findAllByMatchingStatusAndStatus(MatchingStatus matchingStatus, int offset, int limit, EntityStatus status) {
         return matchingApplicationQueryDslRepository.findAllByMatchingStatusAndStatus(matchingStatus, offset, limit, status);
     }
 
-    @Override
     public long countByMatchingStatusAndStatus(MatchingStatus matchingStatus, EntityStatus status) {
         return matchingApplicationAdminJpaRepository.countByMatchingStatusAndStatus(matchingStatus, status);
     }
--- src/test/java/org/kwakmunsu/randsome/admin/matching/service/MatchingAdminServiceTest.java
+++ src/test/java/org/kwakmunsu/randsome/admin/matching/service/MatchingAdminServiceTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kwakmunsu.randsome.admin.matching.repository.matching.MatchingAdminRepository;
+import org.kwakmunsu.randsome.admin.matching.repository.matchingapplication.MatchingApplicationAdminRepository;
 import org.kwakmunsu.randsome.domain.matching.entity.Matching;
 import org.kwakmunsu.randsome.domain.matching.entity.MatchingApplication;
 import org.kwakmunsu.randsome.domain.matching.enums.MatchingStatus;
