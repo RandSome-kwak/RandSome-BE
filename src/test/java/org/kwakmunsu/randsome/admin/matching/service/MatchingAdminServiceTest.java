b496e22cdf7f295c76bcb83da6a77116ae7f73f6
kwakseobang <iii6602@naver.com>
2025-10-24 09:45:34 +0900

    Refactor[#92]: Rename MatchingAdminRepositoryImpl to MatchingAdminRepository and update imports for consistency

--- src/test/java/org/kwakmunsu/randsome/admin/matching/service/MatchingAdminServiceTest.java
+++ src/test/java/org/kwakmunsu/randsome/admin/matching/service/MatchingAdminServiceTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kwakmunsu.randsome.admin.matching.repository.matching.MatchingAdminRepository;
 import org.kwakmunsu.randsome.domain.matching.entity.Matching;
 import org.kwakmunsu.randsome.domain.matching.entity.MatchingApplication;
 import org.kwakmunsu.randsome.domain.matching.enums.MatchingStatus;
