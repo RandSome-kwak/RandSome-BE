652d21bea985f7876e98da2beb6d70b5867ee138
kwakseobang <iii6602@naver.com>
2025-10-24 10:02:40 +0900

    Refactor[#92]: Rename MatchingRepositoryImpl to MatchingRepository and update imports for consistency

--- src/main/java/org/kwakmunsu/randsome/domain/matching/repository/MatchingRepository.java
+++ src/main/java/org/kwakmunsu/randsome/domain/matching/repository/MatchingRepository.java
@@ -2,16 +2,14 @@ package org.kwakmunsu.randsome.domain.matching.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.kwakmunsu.randsome.domain.EntityStatus;
-import org.kwakmunsu.randsome.domain.matching.service.MatchingRepository;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
-public class MatchingRepositoryImpl implements MatchingRepository {
+public class MatchingRepository {
 
     private final MatchingJpaRepository matchingJpaRepository;
 
-    @Override
     public long countBySelectedMemberIdAndStatus(Long selectedMemberId, EntityStatus status) {
         return matchingJpaRepository.countBySelectedMemberIdAndStatus(selectedMemberId, status);
     }
